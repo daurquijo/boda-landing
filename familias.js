4fb624700fe009fdb66614cdd53266148d97cac8
david.urquijo@sainapsis.com <tu-email@dominio.com>
2026-09-20 21:11:20 -0500

    add validation

--- familias.js
+++ familias.js
@@ -30,5 +30,4 @@ export const FAMILIAS = [
   { id:"monica-estrada",      nombre:"Monica Estrada",              miembros:["Monica Estrada"] },
   { id:"lorena-jaramillo",    nombre:"Lorena Jaramillo",            miembros:["Lorena Jaramillo"] },
   { id:"silva",               nombre:"Familia Silva",               miembros:["Santiago Silva","Natalia"] },
-  { id: "leon-moon",          nombre: "Leon Moon",                  miembros: ["Leon Moon"] },
 ];
